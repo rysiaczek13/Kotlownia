aa6562f81ac427d2d02af99f13bb57d76af8903f
ryszard wojtas <ryszard.wojtas@gmail.com>
2019-10-28 17:27:55 +0100

    Added email notification

--- Kotlownia.ino
+++ Kotlownia.ino
@@ -2,16 +2,16 @@
 #include <ESP8266WiFi.h>
 #include <OneWire.h>
 #include <SimpleTimer.h>
-#define BLYNK_PRINT Serial         // Comment this out to disable prints and save space
 #include <BlynkSimpleEsp8266.h>
 
-//pin number where sensor is connected
-#define ONEWIRE_PIN D2
+#define BLYNK_PRINT Serial         // Comment this out to disable prints and save space
+#define ONEWIRE_PIN D6             //pin number where sensor is connected
 
-const char* ssid = "domeczek";
-const char* password = "Olineczek2010";
+const char* ssid = "SSID";
+const char* password = "Password";
 char auth[] = "5ecc693b3675476e81f2923c128b44be";
 float temperature;
+bool emailSendedToReceiver = false;
 
 OneWire oneWire(ONEWIRE_PIN);
 DallasTemperature sensors(&oneWire);
@@ -34,10 +34,24 @@ void loop() {
 
 void sendTemps()
 {
-  sensors.requestTemperatures();                  // Polls the sensors.
+  sensors.requestTemperatures();              // Polls the sensors.
   temperature = sensors.getTempCByIndex(0);   // Stores temperature. Change to getTempCByIndex(0) for celcius.
   Blynk.virtualWrite(1, temperature);         // Send temperature to Blynk app virtual pin 1.
-
+  SendEmailIfNeeded();
   Serial.print(temperature);
   Serial.println(F(" 'C"));
+}
+
+void SendEmailIfNeeded() {
+  if(temperature <= 35.0 && !emailSendedToReceiver)
+  {
+    Blynk.email("email_address", "Alert Temperatury w zasobniku", "Temperatura w zasobniku spadła poniżej 32 stopni.");
+    emailSendedToReceiver = true;
+  }
+
+  if(temperature >= 45.0 && emailSendedToReceiver)
+  {
+    Blynk.email("email_address", "Temperatura w zasobniku w normie", "Temperatura w zasobniku wróciła powyżej 42 stopni.");
+    emailSendedToReceiver = false;
+  }
 }
